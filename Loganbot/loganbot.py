3761bdef766f89ff5fe5e6525138e21f6b27e912
Logan <45982289+MegaWeebLogan@users.noreply.github.com>
2020-10-11 13:41:03 -0400

    Update loganbot.py

--- Loganbot/loganbot.py
+++ Loganbot/loganbot.py
@@ -4,7 +4,7 @@ from discord.ext import commands
 import asyncio
 import os
 import random
-import youtube_dl
+
 client = commands.Bot(command_prefix = '.')
 
 @client.command()
@@ -136,77 +136,4 @@ async def hug(ctx, *, user):
 	path = './cogs/hug/' + imgString
 	await ctx.send(f'{ctx.message.author.mention} huged {user}!', file=discord.File(path))
 
-@client.command(name='join', help='This command makes the bot join the voice channel')
-async def join(ctx):
-    if not ctx.message.author.voice:
-        await ctx.send("You are not connected to a voice channel")
-        return
-    
-    else:
-        channel = ctx.message.author.voice.channel
-
-    await channel.connect()
-
-@client.command(name='queue', help='This command adds a song to the queue')
-async def queue_(ctx, url):
-    global queue
-
-    queue.append(url)
-    await ctx.send(f'`{url}` added to queue!')
-
-@client.command(name='remove', help='This command removes an item from the list')
-async def remove(ctx, number):
-    global queue
-
-    try:
-        del(queue[int(number)])
-        await ctx.send(f'Your queue is now `{queue}!`')
-    
-    except:
-        await ctx.send('Your queue is either **empty** or the index is **out of range**')
-        
-@client.command(name='play', help='This command plays songs')
-async def play(ctx):
-    global queue
-
-    server = ctx.message.guild
-    voice_channel = server.voice_client
-
-    async with ctx.typing():
-        player = await YTDLSource.from_url(queue[0], loop=client.loop)
-        voice_channel.play(player, after=lambda e: print('Player error: %s' % e) if e else None)
-
-    await ctx.send('**Now playing:** {}'.format(player.title))
-    del(queue[0])
-
-@client.command(name='pause', help='This command pauses the song')
-async def pause(ctx):
-    server = ctx.message.guild
-    voice_channel = server.voice_client
-
-    voice_channel.pause()
-
-@client.command(name='resume', help='This command resumes the song!')
-async def resume(ctx):
-    server = ctx.message.guild
-    voice_channel = server.voice_client
-
-    voice_channel.resume()
-
-@client.command(name='view', help='This command shows the queue')
-async def view(ctx):
-    await ctx.send(f'Your queue is now `{queue}!`')
-
-@client.command(name='leave', help='This command stops makes the bot leave the voice channel')
-async def leave(ctx):
-    voice_client = ctx.message.guild.voice_client
-    await voice_client.disconnect()
-
-@client.command(name='stop', help='This command stops the song!')
-async def stop(ctx):
-    server = ctx.message.guild
-    voice_channel = server.voice_client
-
-    voice_channel.stop()
-
-client.run('NzQ4NjgzMzE0MTIyMTk1MTI0.X0g_qw.IervcTvqSJ16Bw6wX5GFrFWXuwg') 
+client.run('NzQ4NjgzMzE0MTIyMTk1MTI0.X0g_qw.IervcTvqSJ16Bw6wX5GFrFWXuwg')
